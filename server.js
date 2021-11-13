1d667086bb0e6f467e74eba9030e74b7f88bcb46
julikss <julie.lehenka@gmail.com>
2021-11-13 17:44:11 +0200

    added dependencies

---
+++ server.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+//run when client connects
+io.on('connect', socket => {
+  console.log('New client connected...')
+})
+
+const PORT = 3000 || process.env.PORT;
+
+app.listen(PORT, () => `Server is running at ${PORT}`);
+
+
+
