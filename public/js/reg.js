2284f5b95bb13d584398e19f76289508bd62f36a
Ukioxz <ikatember@gmail.com>
2021-12-10 22:19:39 +0200

    registration

---
+++ public/js/reg.js
@@ -0,0 +1,19 @@
+const form = document.getElementById('reg-form');
+form.addEventListener('submit', registerUser);
+
+async function registerUser(event) {
+  event.preventDefault()
+  const username = document.getElementById('username')
+  const passwd = document.getElementById('passwd')
+
+  const result = await fetch('/api/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username,
+      passwd
+    })
+  }).then((res) => res.json())
+}
