d86e8e36d5d35f09c99536561af7ff051fae6b06
Anastasiia Sanina <saninaanstasia97@gmail.com>
2021-12-11 22:36:00 +0200

    authentification

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
+const mongoo = require('mongoose');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,27 +10,31 @@ const io = socketio(server);
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
- 
 
 //run when client connects
 io.on('connection', socket => {
-  
-  socket.emit('message', 'Welcome to messenger');
 
-  //broadcast when a user connects
-  socket.broadcast.emit('message', 'A user has joined the chat');
+    socket.emit('message', 'Welcome to messenger');
 
-  //runs when client disconnects
-  socket.on('disconnect', () =>{
-    io.emit('message', 'A user has left the chat')
-  })
+    //broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    //runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat')
+    })
+});
+
+
+app.post('/api/login', async(request, response) => {
+    const { username, password } = request.body;
+
+
+
 });
 
 const PORT = 3000 || process.env.PORT;
 
 server.listen(PORT, () => {
-console.log(`Server is running at ${PORT}`)
+    console.log(`Server is running at ${PORT}`)
 });
-
-
-
