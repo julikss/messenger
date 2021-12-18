49b174ce51d86488d62422d35d8385cd89fa8742
Anastasiia Sanina <saninaanstasia97@gmail.com>
2021-12-18 15:45:32 +0200

    request

---
+++ public/js/login.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('login');
+
+const loginUser = async(event) => {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const result = await fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    }).then(res => res.json());
+
+    console.log(result);
+
+}
+
+form.addEventListener('click', loginUser);
