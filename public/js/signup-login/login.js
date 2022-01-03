8dd690faf04cff2c22314e597d35130b2b8a0900
Anastasiia Sanina <saninaanstasia97@gmail.com>
2022-01-04 01:49:24 +0200

    fix login

--- public/js/signup-login/login.js
+++ public/js/signup-login/login.js
@@ -1,11 +1,11 @@
 const form = document.getElementById('login');
 
-/*const loginUser = async(event) => {
+const loginUser = async(event) => {
     event.preventDefault();
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    const result = await fetch('/api/login', {
+    const result = await fetch('/auth/api/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -18,26 +18,8 @@ const form = document.getElementById('login');
 
     console.log(result);
 
-}*/
+}
 
 if (form) {
-    form.addEventListener('click', async(event) => {
-        event.preventDefault();
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
-
-        const result = await fetch('/api/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username,
-                password
-            })
-        }).then(res => res.json());
-
-        console.log(result);
-
-    });
+    form.addEventListener('click', loginUser);
 }
--- public/js/signup-login/reg.js
+++ public/js/signup-login/reg.js
@@ -1,20 +1,23 @@
 const form = document.getElementById('reg-form');
-form.addEventListener('submit', registerUser);
 
-async function registerUser(event) {
-  event.preventDefault()
-  const username = document.getElementById('username').value
-  const passwd = document.getElementById('passwd').value
+const registerUser = async(event) => {
+    event.preventDefault()
+    const username = document.getElementById('username').value
+    const passwd = document.getElementById('passwd').value
 
-  const result = await fetch('/api/reg', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      username,
-      passwd
-    })
-  }).then((res) => res.json())
+    const result = await fetch('/registration', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username,
+            passwd
+        })
+    }).then((res) => res.json())
 
+}
+
+if (form) {
+    form.addEventListener('submit', registerUser);
 }
