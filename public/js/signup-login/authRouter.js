2d32de2f2726ba2666e4b9800d215dd1e0f35a64
ukioxz <ikatember@gmail.com>
2022-01-04 09:36:32 +0200

    email reg

--- public/js/signup-login/authRouter.js
+++ public/js/signup-login/authRouter.js
@@ -5,6 +5,7 @@ const { check } = require('express-validator');
 
 router.post('/registration',
     check('username', "cannot be empty").notEmpty(),
+    check('email', "cannot be empty").isEmail(),
     check('password', "too small/too long").isLength({ min: 4, max: 12 }),
     controller.registration);
 router.post('/api/login', controller.login);
