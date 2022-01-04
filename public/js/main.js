a7f308467e727ca2b283ec209775b644761a8ca2
Anastasiiiii <nastia262002@gmail.com>
2022-01-04 16:29:56 +0200

    chat

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,26 @@
 'use strict';
 
+const chatForm = document.getElementById('chat_form');
+const chatMessages = document.querySelector('.chat-messages');
 // eslint-disable-next-line no-undef
 const socket = io();
 
 socket.on('message', message => {
-  console.log(message);
+    console.log(message);
+});
+
+chatForm.addEventListener('submit', (send) => {
+    send.preventDefault();
+
+    let message = send.target.elements.message.valuel;
+    message = message.trim();
+
+    if (!message) {
+        return false;
+    }
+
+    socket.email('MessageInChat', message);
+
+    send.target.elements.message.value = '';
+    send.target.elements.message.focus();
 });
