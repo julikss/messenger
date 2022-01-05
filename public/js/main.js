5533773dcfe5425f19b97400e4700e2b43a54673
Anastasiiiii <nastia262002@gmail.com>
2022-01-05 23:04:00 +0200

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,40 @@
 'use strict';
 
 const chatForm = document.getElementById('chat_form');
-const chatMessages = document.querySelector('.chat-messages');
+const chatMessages = document.querySelector('.chat');
 // eslint-disable-next-line no-undef
 const socket = io("ws://localhost:3000");
 
 socket.on('message', message => {
     console.log(message);
+    outputMessage(message);
 });
 
 chatForm.addEventListener('submit', (send) => {
     send.preventDefault();
 
-    let message = send.target.elements.message.valuel;
-    message = message.trim();
+    let msg = send.target.elements.msg.value;
+    msg = msg.trim();
 
-    if (!message) {
+    if (!msg) {
         return false;
     }
 
-    socket.email('MessageInChat', message);
+    socket.emit('MessageInChat', msg);
 
-    send.target.elements.message.value = '';
-    send.target.elements.message.focus();
+    send.target.elements.msg.value = '';
+    send.target.elements.msg.focus();
 });
+
+
+// Output message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message1');
+    div.innerHTML = `<p class="text">${message}</p>`;
+    // const p = document.createElement('p');
+    // p.classList.add('text');
+    // p.innerText = message.text;
+    // div.appendChild(p);
+    document.querySelector('.chat').appendChild(div);
+}
