f8eccff1dcdbed97b9850269fdb541d51376cfd0
Anastasiia Sanina <saninaanstasia97@gmail.com>
2022-01-05 21:53:50 +0200

    add chat-bot

--- public/js/notes/notes.js
+++ public/js/notes/notes.js
@@ -1,141 +1,139 @@
 let addNote = document.querySelector('.message'),
-	addButton = document.querySelector('.add'),
-	todo = document.querySelector('.todo'), 
-	del = document.getElementById('delete'),
-	clear = document.getElementById('clear'),
-	mark = document.getElementById('mark'),
-	copy = document.getElementById('copy'),
-	edit = document.getElementById('edit');
+    addButton = document.querySelector('.add'),
+    todo = document.querySelector('.todo'),
+    del = document.getElementById('delete'),
+    clear = document.getElementById('clear'),
+    mark = document.getElementById('mark'),
+    copy = document.getElementById('copy'),
+    edit = document.getElementById('edit');
 
 let todoList = [];
 let menu = null;
 
 
-document.addEventListener('DOMContentLoaded', function(){
-	 
-	menu = document.querySelector('.menu');
-	menu.classList.add('off');
-	menu.addEventListener('mouseleave', hideMenu);
+document.addEventListener('DOMContentLoaded', function() {
+
+    menu = document.querySelector('.menu');
+    menu.classList.add('off');
+    menu.addEventListener('mouseleave', hideMenu);
 
 });
 
 
-if(localStorage.getItem('todo')){
-	todoList = JSON.parse(localStorage.getItem('todo'));
-	displayMessage();
+if (localStorage.getItem('todo')) {
+    todoList = JSON.parse(localStorage.getItem('todo'));
+    displayMessage();
 }
 
 addButton.addEventListener('click', () => {
-	if(!addNote.value) return;
+    if (!addNote.value) return;
 
-		let newTodo = {
-			todo: addNote.value,
-			checked: false,
-			important: false
-		};
+    let newTodo = {
+        todo: addNote.value,
+        checked: false,
+        important: false
+    };
 
-		todoList.push(newTodo);
-		displayMessage();
-		localStorage.setItem('todo', JSON.stringify(todoList));
-		addNote.value = '';
-	});
+    todoList.push(newTodo);
+    displayMessage();
+    localStorage.setItem('todo', JSON.stringify(todoList));
+    addNote.value = '';
+});
 
-function displayMessage(){
-	let displayMessage = '';
-	if(todoList.length === 0) todo.innerHTML = '';
+function displayMessage() {
+    let displayMessage = '';
+    if (todoList.length === 0) todo.innerHTML = '';
 
-	//for (const todo of todoList) {
-		//for (let i=0; i<todoList.length; i++) {
-			todoList.forEach(function(item, i){
-				displayMessage += `
+    //for (const todo of todoList) {
+    //for (let i=0; i<todoList.length; i++) {
+    todoList.forEach(function(item, i) {
+        displayMessage += `
 				<li>
 				<input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
 				<label for='item_${i}' class='${item.important ? 'important' : ''}'>${item.todo}</label>
 				</li>
 				`;
-				todo.innerHTML = displayMessage;
-			});
+        todo.innerHTML = displayMessage;
+    });
 }
 
-todo.addEventListener('change', function(event){
-	let idInput = event.target.getAttribute('id');
-	let forLabel = todo.querySelector('[for='+ idInput +']');
-	let valueLabel = forLabel.innerHTML;
+todo.addEventListener('change', function(event) {
+    let idInput = event.target.getAttribute('id');
+    let forLabel = todo.querySelector('[for=' + idInput + ']');
+    let valueLabel = forLabel.innerHTML;
 
-	todoList.forEach(function(item){
-		if (item.todo === valueLabel) {
-			item.checked =! item.checked;
-			localStorage.setItem('todo', JSON.stringify(todoList));
-		}
-	});
+    todoList.forEach(function(item) {
+        if (item.todo === valueLabel) {
+            item.checked = !item.checked;
+            localStorage.setItem('todo', JSON.stringify(todoList));
+        }
+    });
 });
 
 
-todo.addEventListener('contextmenu', function(e){
+todo.addEventListener('contextmenu', function(e) {
 
-	e.preventDefault();
-	todo.addEventListener('contextmenu', displayMenu);
-	let text = e.target.innerHTML;
+    e.preventDefault();
+    todo.addEventListener('contextmenu', displayMenu);
+    let text = e.target.innerHTML;
 
-	//delete note
-	del.addEventListener('click', function(e){
+    //delete note
+    del.addEventListener('click', function(e) {
 
-		todoList.forEach(function(item, i){
-		if(item.todo === text) {
-			todoList.splice(i, 1);
-			displayMessage();
-			localStorage.setItem('todo', JSON.stringify(todoList));
-		}
-		})
-	});
+        todoList.forEach(function(item, i) {
+            if (item.todo === text) {
+                todoList.splice(i, 1);
+                displayMessage();
+                localStorage.setItem('todo', JSON.stringify(todoList));
+            }
+        })
+    });
 
-	//clear note
-	clear.addEventListener('click', function(e) {
-		for(const item of todoList) {
-			todoList.splice(todoList.indexOf(item));
-			displayMessage();
-			localStorage.setItem('todo', JSON.stringify(todoList));
-		}
-	});
+    //clear note
+    clear.addEventListener('click', function(e) {
+        for (const item of todoList) {
+            todoList.splice(todoList.indexOf(item));
+            displayMessage();
+            localStorage.setItem('todo', JSON.stringify(todoList));
+        }
+    });
 
-	//mark as important
-	mark.addEventListener('click', function(e){
-		for(const item of todoList){
-			if(item.todo === text) {
-				item.important =! item.important;
-				displayMessage();
-				localStorage.setItem('todo', JSON.stringify(todoList));
-			}
-		}
-	});
+    //mark as important
+    mark.addEventListener('click', function(e) {
+        for (const item of todoList) {
+            if (item.todo === text) {
+                item.important = !item.important;
+                displayMessage();
+                localStorage.setItem('todo', JSON.stringify(todoList));
+            }
+        }
+    });
 
-	//copy text 
-	copy.addEventListener('click',  function(){
-		navigator.clipboard.writeText(text);
-	});
+    //copy text 
+    copy.addEventListener('click', function() {
+        navigator.clipboard.writeText(text);
+    });
 
-	//edit text 
-	edit.addEventListener('click', function(){
-		
-	})
-		
+    //edit text 
+    edit.addEventListener('click', function() {
+
+    })
+
 })
 
 
-function displayMenu(e){
-	e.preventDefault(); 
-	menu.style.top = `${e.clientY - 20}px`;
-	menu.style.left = `${e.clientX - 20}px`;
-	menu.classList.remove('off');
+function displayMenu(e) {
+    e.preventDefault();
+    menu.style.top = `${e.clientY - 20}px`;
+    menu.style.left = `${e.clientX - 20}px`;
+    menu.classList.remove('off');
 }
 
-function hideMenu(e){
-	menu.classList.add('off');
-	menu.style.top = '-200%';
-	menu.style.left = '-200%';
+function hideMenu(e) {
+    menu.classList.add('off');
+    menu.style.top = '-200%';
+    menu.style.left = '-200%';
 }
-
-
 
 const highlightNote = (note, pos, length) => {
     return note.slice(0, pos) + '<mark>' +
@@ -152,7 +150,8 @@ document.querySelector('#input').oninput = function() {
             if (el.innerText.includes(searchData)) {
                 let str = el.innerText;
                 //console.log(el);
-                el.innerHTML = highlightNote(str, el.innerText.search(searchData), searchData.length);
+                const position = el.innerText.search(searchData);
+                el.innerHTML = highlightNote(str, position, searchData.length);
             } else {
                 el.innerHTML = el.innerText;
             }
